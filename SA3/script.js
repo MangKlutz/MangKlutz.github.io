a0686e8ae8a34af12e6b4950bd76fa001cfaaa41
MangKlutz <167587764+MangKlutz@users.noreply.github.com>
2024-06-22 00:05:03 +0800

    HEY

---
+++ SA3/script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+    $('#welcomeModal').modal('show');
+  
+    let basicClicked = false;
+    let premiumClicked = false;
+    let vipClicked = false;
+  
+    document.getElementById("basic-img").addEventListener("click", function() {
+      basicClicked = true;
+      checkIfAllClicked();
+    });
+  
+    document.getElementById("premium-img").addEventListener("click", function() {
+      premiumClicked = true;
+      checkIfAllClicked();
+    });
+  
+    document.getElementById("vip-img").addEventListener("click", function() {
+      vipClicked = true;
+      checkIfAllClicked();
+    });
+  
+    function checkIfAllClicked() {
+      if (basicClicked && premiumClicked && vipClicked) {
+        alert("You must be signed in to access this feature.");
+      }
+    }
+  });
+  
